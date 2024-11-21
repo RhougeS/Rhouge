e0f01c8ee6e39d6d641beb643ee9b2a0cd81f5a8
Grocklh <gothenfant@gmail.com>
2024-11-21 19:19:17 +0000

    Create ai.js
    Signed-off-by: Grocklh <gothenfant@gmail.com>

---
+++ scripts/cmds/ai.js
@@ -0,0 +1,60 @@
+const axios = require('axios');
+
+async function fetchFromAI(url, params) {
+ try {
+ const response = await axios.get(url, { params });
+ return response.data;
+ } catch (error) {
+ console.error(error);
+ return null;
+ }
+}
+
+async function getAIResponse(input, userName, userId, messageID) {
+ const services = [
+ { url: 'https://ai-chat-gpt-4-lite.onrender.com/api/hercai', params: { question: input } }
+ ];
+
+ let response = ` 𝐌𝐞 𝐕𝐨𝐢𝐜𝐢..`;
+ let currentIndex = 0;
+
+ for (let i = 0; i < services.length; i++) {
+ const service = services[currentIndex];
+ const data = await fetchFromAI(service.url, service.params);
+ if (data && (data.gpt4 || data.reply || data.response)) {
+ response = data.gpt4 || data.reply || data.response;
+ break;
+ }
+ currentIndex = (currentIndex + 1) % services.length; // Passer au service suivant
+ }
+
+ return { response, messageID };
+}
+module.exports = {
+  config: {
+    name: 'ai',
+    author: 'Arn',
+    role: 0,
+    category: 'ai',
+    shortDescription: 'ai to ask anything',
+  },
+  onStart: async function ({ api, event, args }) {
+    const input = args.join(' ').trim();
+    if (!input) {
+      api.sendMessage(`𝐌𝐞 𝐕𝐨𝐢𝐜𝐢..`, event.threadID, event.messageID);
+      return;
+    }
+
+    const { response, messageID } = await getAIResponse(input, event.senderID, event.messageID);
+    api.sendMessage(`${response}`, event.threadID, messageID);
+  },
+  onChat: async function ({ event, message }) {
+    const messageContent = event.body.trim().toLowerCase();
+    if (messageContent.startsWith("ai")) {
+      const input = messageContent.replace(/^ai\s*/, "").trim();
+      const { response, messageID } = await getAIResponse(input, event.senderID, message.messageID);
+      message.reply(`⚜🌹...............................\n${response} \n ⚜🌹...............................`, messageID); 
+api.setMessageReaction("💬", event.messageID, () => {}, true);
+    }
+  }
+};
